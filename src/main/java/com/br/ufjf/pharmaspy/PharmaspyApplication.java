e6524af987f3dd81f51191a4c2f276ac6c9225c9
JoaoPDias <jpdias1997@hotmail.com>
2019-05-10 21:14:54 -0300

    Commit Inicial

---
+++ src/main/java/com/br/ufjf/pharmaspy/PharmaspyApplication.java
@@ -0,0 +1,13 @@
+package com.br.ufjf.pharmaspy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaspyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaspyApplication.class, args);
+    }
+
+}
