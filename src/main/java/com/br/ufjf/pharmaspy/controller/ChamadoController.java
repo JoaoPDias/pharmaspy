08065f8cbd125f607046d7f5ca6df61e9f6d67c1
Rian Alves <rianalves2009.ra@gmail.com>
2019-05-12 15:59:43 -0300

    Implementação chamado.jsp

--- src/main/java/com/br/ufjf/pharmaspy/controller/ChamadoController.java
+++ src/main/java/com/br/ufjf/pharmaspy/controller/ChamadoController.java
@@ -1,18 +1,15 @@
 package com.br.ufjf.pharmaspy.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class UsuarioController {
-
-    @RequestMapping("chamado.html")
-    public ModelAndView chamado ()
-    {
+public class ChamadoController {
+    @GetMapping({"chamado.html"})
+    public ModelAndView chamado() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("chamado");
         return mv;
     }
-
 }
