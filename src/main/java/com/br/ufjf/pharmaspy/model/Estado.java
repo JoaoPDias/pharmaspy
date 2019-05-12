e9bfe743a520964129f45bac7f1af701b8d8aade
Rian Alves <rianalves2009.ra@gmail.com>
2019-05-12 12:16:36 -0300

    Criação de Enum Estado

---
+++ src/main/java/com/br/ufjf/pharmaspy/model/Estado.java
@@ -0,0 +1,10 @@
+package com.br.ufjf.pharmaspy.model;
+
+public enum Estado {
+    RJ("RJ"),SP("SP"),MG("MG");
+    private String estado;
+
+    Estado(String estado) {
+        this.estado = estado;
+    }
+}
