3cd5d5a29605e876d87c49d9f788ece3504cbbd7
JoaoPDias <joao.dias@delage.com.br>
2019-05-12 14:41:24 -0300

    Correção da Imagem Inicial

--- src/main/java/com/br/ufjf/pharmaspy/PharmaspyApplication.java
+++ src/main/java/com/br/ufjf/pharmaspy/PharmaspyApplication.java
@@ -2,10 +2,20 @@ package com.br.ufjf.pharmaspy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class PharmaspyApplication {
-
+public class PharmaspyApplication implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Register resource handler for images
+        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+    }
     public static void main(String[] args) {
         SpringApplication.run(PharmaspyApplication.class, args);
     }
