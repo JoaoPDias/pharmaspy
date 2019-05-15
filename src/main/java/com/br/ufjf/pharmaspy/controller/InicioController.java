e8c6c0c22dd83cc67f34124306b309f0773de689
JoaoPDias <jpdias1997@hotmail.com>
2019-05-15 02:57:27 -0300

    Adição dos Efeitos Adversos

--- src/main/java/com/br/ufjf/pharmaspy/controller/InicioController.java
+++ src/main/java/com/br/ufjf/pharmaspy/controller/InicioController.java
@@ -44,4 +44,11 @@ public class InicioController {
         return mv;
     }
 
+    @GetMapping("graficopizza.html")
+    public ModelAndView pizza(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("graficopizza");
+        return mv;
+    }
+
 }
